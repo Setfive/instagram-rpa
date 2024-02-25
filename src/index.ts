9a5292fb083bc50d414fd0ec4cd7764e2d7be294
Ashish Datta <ashish@setfive.com>
2024-02-24 19:32:04 -0500

    Wired up fetching photos off the grid

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import puppeteer from 'puppeteer';
 import * as fs from 'fs';
 import 'dotenv/config'
-import {SEARCH_BTN_SELECTOR, SEARCH_FIELD_SELECTOR, SEARCH_LINK} from "./selectors";
+import {
+  CLOSE_BTN, NEXT_BTN,
+  PHOTO_GRID,
+  PHOTO_GRID_USERNAME, PHOTO_GRID_USERNAME_ONE, PHOTO_GRID_USERNAME_TWO,
+  SEARCH_BTN_SELECTOR,
+  SEARCH_FIELD_SELECTOR,
+  SEARCH_LINK
+} from "./selectors";
 
 (async () => {
   const INSTAGRAM_USERNAME = process.env.INSTAGRAM_USERNAME;
@@ -70,7 +77,45 @@ import {SEARCH_BTN_SELECTOR, SEARCH_FIELD_SELECTOR, SEARCH_LINK} from "./selecto
   console.log('Waiting for search link...');
   const searchLink = await page.waitForSelector(SEARCH_LINK, {timeout: 30 * 1000});
   await searchLink.click();
-  
+
+  console.log('Waiting for grid');
+  await page.waitForSelector(PHOTO_GRID, {timeout: 30 * 1000});
+  const photoGridDivs = await page.$$(PHOTO_GRID);
+
+  const capturedUsernames: string[] = [];
+  console.log('Clicking photos...');
+
+  const a = await photoGridDivs[0].$('a');
+  await a.click();
+
+  for(let i = 0; i < 100; i++) {
+    try {
+      const usernameAhref = await page.waitForSelector(PHOTO_GRID_USERNAME, {timeout: 1 * 1000});
+      const username = await usernameAhref.evaluate(el => el.textContent, usernameAhref);
+      console.log(`username = ${username}`);
+      capturedUsernames.push(username);
+    }catch(e) {
+      const usernameOneAhref = await page.waitForSelector(PHOTO_GRID_USERNAME_ONE, {timeout: 1 * 1000});
+      const usernameOne = await usernameOneAhref.evaluate(el => el.textContent, usernameOneAhref);
+
+      const usernameTwoAhref = await page.waitForSelector(PHOTO_GRID_USERNAME_TWO, {timeout: 1 * 1000});
+      const usernameTwo = await usernameTwoAhref.evaluate(el => el.textContent, usernameTwoAhref);
+
+      console.log(`usernameOne = ${usernameOne}, usernameTwo = ${usernameTwo}`);
+
+      capturedUsernames.push(usernameOne);
+      capturedUsernames.push(usernameTwo);
+    }
+
+    await sleep(1);
+
+    console.log('Waiting for next btn...');
+    const nextBtn = await page.waitForSelector(NEXT_BTN, {timeout: 30 * 1000});
+    await nextBtn.click();
+
+    await sleep(1);
+  }
+
 })();
 
 async function sleep(time) {
